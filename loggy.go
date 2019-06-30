65007a3cd8e996510b37b26d27af554e8c61eaa7
Seth Hoenig <hoenig@indeed.com>
2019-06-30 10:22:46 -0500

    gh-1 : provide a configurable constructor

--- loggy.go
+++ loggy.go
@@ -1,14 +1,15 @@
-// Package loggy provides a simple logging package.
+// Package loggy provides a simple logging library for Go.
 package loggy // import "go.gophers.dev/pkgs/loggy"
 
 import (
-	"io/ioutil"
 	"log"
 	"os"
 )
 
-// A Logger is used to write log lines to an underlying log.Logger, but with
-// Trace/Info/Warn/Error levels and package prefix names.
+// A Logger is used to write log lines.
+//
+// There are four methods for outputting a log message, each being associated
+// with a different degree of urgency.
 type Logger interface {
 	Tracef(string, ...interface{})
 	Infof(string, ...interface{})
@@ -17,24 +18,63 @@ type Logger interface {
 }
 
 type logger struct {
-	ll  *log.Logger
-	tag string
+	tag    string
+	output Printer
 }
 
-// New creates a Logger which prepends each statement with the specified prefix.
+// A Printer is anything that can be used to Printf log messages.
+//
+// The default underlying implementation of Logger provided by the loggy
+// package defers to the standard library log package, which in turn provides
+// an implementation of Printer that writes to an output file.
+type Printer interface {
+	Printf(string, ...interface{})
+}
+
+// Open creates a standard library *log.Log with the given file as the
+// underlying output device.
+func Open(file *os.File) Printer {
+	return log.New(file, "", log.LstdFlags)
+}
+
+func tag(prefix string) string {
+	return "[" + prefix + "]"
+}
+
+// Options allow for configuring a Logger, with the given Prefix and Output
+// mechanism.
+type Options struct {
+	Prefix string
+	Output Printer
+}
+
+// New creates a Logger which sends output to os.Stderr with each log line
+// tagged with the given prefix.
+//
+// To control the output destination, use Configure to create the Logger instead.
 func New(prefix string) Logger {
-	tag := "[" + prefix + "] "
+	return Configure(Options{
+		Prefix: prefix,
+		Output: Open(os.Stderr),
+	})
+}
+
+// Configure creates a Logger with the configured Options.
+//
+// This allows for controlling the output destination of the created Logger.
+func Configure(opts Options) Logger {
 	return &logger{
-		tag: tag,
-		ll:  log.New(os.Stderr, "", log.LstdFlags),
+		tag:    tag(opts.Prefix),
+		output: opts.Output,
 	}
 }
 
 // Discard creates a Logger which throws away each statement.
 func Discard() Logger {
-	return &logger{
-		ll: log.New(ioutil.Discard, "", 0),
-	}
+	devNull := os.NewFile(0, os.DevNull)
+	return Configure(Options{
+		Output: Open(devNull),
+	})
 }
 
 func (l logger) Tracef(format string, a ...interface{}) {
@@ -54,6 +94,6 @@ func (l logger) Errorf(format string, a ...interface{}) {
 }
 
 func (l logger) printf(level, format string, a ...interface{}) {
-	prefixedFmt := level + " " + l.tag + format
-	l.ll.Printf(prefixedFmt, a...)
+	prefixedFmt := level + " " + l.tag + " " + format
+	l.output.Printf(prefixedFmt, a...)
 }
